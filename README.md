5dab56091cfcc531d4be36675a3ea4c5102c79db
Erasmo Jr <erasmo.cjunior@gmail.com>
2022-11-16 13:33:38 -0300

    :tada: Commit inicial e adicao de projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+## Estudos em Kotlin
+
+# CHANGELOG
+## Adicionado
+* bytebanKotlin - alura:Curso de formacao Kotlin
---
+++ bytebanKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    println("Bem vindo ao ByteBank")
+    val titular = "Erasmo"
+    println("titular $titular")
+}
